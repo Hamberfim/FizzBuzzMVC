c8e0fd14fff270994999db919398e1f635153cc9
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-10 11:53:38 -0500

    set up for form

--- FizzBuzzMVC/Models/FBPage.cs
+++ FizzBuzzMVC/Models/FBPage.cs
@@ -2,5 +2,14 @@
 {
     public class FBPage
     {
+        public int MyProperty { get; set; }
+
+        public bool IsFizzBuzz { get; set; }
+        // return a css class parameter
+        public string GetPaliMessClass()
+        {
+            return IsFizzBuzz ? "text-success" : "text-danger";
+        }
+
     }
 }
